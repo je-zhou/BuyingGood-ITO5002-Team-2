58c6dea6a89387d5450129ae6e4dd6d94df1efbe
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2025-07-20 21:18:11 +1000

    Created sign in and sign up routes

---
+++ Frontend/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn, SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen pb-32">
+      <SignIn />
+    </div>
+  );
+}
---
+++ Frontend/components/layout/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center p-4 w-full">
+      {/* Icon and Brand */}
+      <div className="flex items-center gap-2">
+        <Image src="/logo.png" alt="logo" width={32} height={32} />
+        <h1 className="text-2xl ">
+          <span className="text-black/70">Buying</span>
+          <span className="text-primary font-bold">Good</span>
+        </h1>
+      </div>
+
+      {/* Nav Links */}
+      <div className="flex items-center gap-4">
+        <Link href="/about">Our Story</Link>
+        <Link href="/contact">Contact Us</Link>
+      </div>
+
+      {/* CTA button */}
+      <div>
+        <SignedOut>
+          <Link href="/sign-up">
+            <button className="bg-primary text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              I&apos;m a Farmer
+            </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
