169288f08a3d42b34abd2efab9711486ccdacac9
naomiturner23 <ntur0008@student.monash.edu>
2025-07-25 09:44:57 +1000

    Update ABARES_HISTORICAL_REGIONAL_ESTIMATES_DATA_CLEANING.py
    Removed rows and columns not required for analysis

--- ABARES_HISTORICAL_REGIONAL_ESTIMATES_DATA_CLEANING.py
+++ ABARES_HISTORICAL_REGIONAL_ESTIMATES_DATA_CLEANING.py
@@ -1,5 +1,7 @@
 ## DATA CLEANING - ABARES_HISTORICAL_REGIONAL_ESTIMATES
 
+# We want to isolate the total freight costs per area for this dataset
+
 # Import libraries
 import pandas as pd
 
@@ -7,7 +9,7 @@ import pandas as pd
 df = pd.read_csv('ABARES_HISTORICAL_REGIONAL_ESTIMATES.csv')
 
 # Rename columns
-df_clean = df.rename(columns={'Variable': 'variable', 'Year': 'year', 'ABARES region': 'abares_region', 'Value': 'value', 'RSE': 'rse'})
+df_clean = df.rename(columns={'Variable': 'item', 'Year': 'year', 'ABARES region': 'abares_region', 'Value': 'cost', 'RSE': 'rse'})
 
 # Remove duplicates
 df_clean = df_clean.drop_duplicates()
@@ -16,7 +18,13 @@ df_clean = df_clean.drop_duplicates()
 df_clean = df_clean.dropna()
 
 # Set data types
-df_clean = df_clean.astype({'variable': str, 'year': int, 'abares_region': str, 'value': float, 'rse': float})
+df_clean = df_clean.astype({'year': int, 'abares_region': str, 'cost': float})
+
+# Filter by only 'Total freight ($)'
+df_clean = df_clean[df_clean['item']=='Total freight ($)']
+
+# Select only required columns
+df_clean = df_clean[['abares_region','year','cost']]
 
 # Export cleaned data to csv
 df_clean.to_csv('ABARES_HISTORICAL_REGIONAL_ESTIMATES_CLEAN.csv', index = False)
