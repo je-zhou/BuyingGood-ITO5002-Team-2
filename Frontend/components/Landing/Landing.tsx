63abfe6b3d7898b4aadab7746821ded1a95a4c51
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2025-07-25 12:02:17 +1000

    adding testimonials

--- Frontend/components/Landing/Landing.tsx
+++ Frontend/components/Landing/Landing.tsx
@@ -3,6 +3,7 @@ import LandingHero from "./Hero";
 import OurStory from "./OurStory";
 import OurSolution from "./OurSolution";
 import WhatsInItForYou from "./WhatsInItForYou";
+import Testimonials from "./Testimonials";
 
 export default function Landing() {
   return (
@@ -11,6 +12,8 @@ export default function Landing() {
       <div className="flex flex-col gap-72 pb-40">
         <OurStory />
         <OurSolution />
+        <Testimonials />
+
         <WhatsInItForYou />
       </div>
     </div>
