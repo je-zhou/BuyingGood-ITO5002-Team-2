d2c46409327cc56bc39a3ff04a99f5da89da6e72
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2025-07-27 10:04:44 +1000

    Update title to mathc logo

--- Frontend/components/Landing/Hero.tsx
+++ Frontend/components/Landing/Hero.tsx
@@ -7,7 +7,10 @@ export default function LandingHero() {
     <div className="flex flex-col h-screen pb-32 justify-center items-center">
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-4xl font-bold">
-          Welcome to <span className="text-primary">Buying Good</span>
+          Welcome to{" "}
+          <span className="text-black/70">
+            Buying<span className="text-primary">Good</span>
+          </span>
         </h1>
         <p className="text-gray-500 max-w-2xl text-center">
           We are a non-for profit initiative that helps connect you with local
@@ -21,8 +24,8 @@ export default function LandingHero() {
 
         <div className="flex flex-col items-center gap-2 pt-8">
           <p className="text-sm text-gray-500">
-            Are you a farmer?{" "}
-            <Link href="/sign-up" className="text-primary">
+            Are you a farmer?
+            <Link href="/dashboard" className="text-primary">
               Register your farm
             </Link>
           </p>
