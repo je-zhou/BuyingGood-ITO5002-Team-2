eae73fe5287b5d1f0e305da9ac69896314d60d6f
naomiturner23 <ntur0008@student.monash.edu>
2025-07-27 15:10:24 +1000

    Update FNQ_BUSINESS_TRENDS_PLOT.py
    Refined visual

--- FNQ_BUSINESS_TRENDS_PLOT.py
+++ FNQ_BUSINESS_TRENDS_PLOT.py
@@ -25,68 +25,62 @@ df = pd.DataFrame(list(collection.find()))
 # We are only interested in the FNQ Agriculture industry
 df = df[df['industry_code']=='A']
 df = df[df['state']=='Queensland']
-df = df[df.lga_label.isin(['Aurukun', 'Burke', 'Cairns', 'Carpentaria', 'Cassowary Coast', 'Cook', 'Croydon', 'Doomadgee', 'Douglas', 'Etheridge', 'Hope Vale', 'Kowanyama', 'Lockhart River', 'Mapoon', 'Mareeba', 'Mornington', 'Napranum', 'Northern Peninsula Area', 'Pormpuraaw', 'Tablelands', 'Torres', 'Torres Strait Island', 'Weipa', 'Wujal Wujal', 'Yarrabah'])]
+df = df[df.lga_label.isin(['Cairns', 'Cassowary Coast', 'Tablelands','Whitsunday'])]
 
 # Create a year column to be used in our plot
 df['year'] = df.date.dt.year
 df = df[['lga_label', 'state', 'year','total_business_count']]
 
-# Create figure
-fig = px.line(df, x="year", y="total_business_count", color="lga_label",
-              title="Business Trends in FNQ <br><sup><i>Active agricultural businesses at the end of each financial year</i></sup>",
-              labels={"year": "Year", "total_business_count": "Businesses", "lga_label": "Region"},
+# Create stacked bar chart
+fig = px.bar(df, x="year", y="total_business_count", color="lga_label",
+              title="Trends in FNQ Top Fruit and Vegetable Production Areas <br><sup><i>Count of agricultural businesses active at the end of each financial year</i></sup>",
+              labels={"year": "Financial Year", "total_business_count": "Businesses", "lga_label": "Region"},
+              color_discrete_sequence= ['#1f77b4','#ff7f0e','#2ca02c','#9467bd'],
               template="plotly_white")
 
 # Ensure year is shown as a category and ordered correctly
 fig.update_xaxes(type='category', categoryorder='array', categoryarray= ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028'])
 
-# Get unique lga_labels
-lga_labels = df['lga_label'].unique()
-
-# Create dropdown buttons for lga_label
-dropdown_buttons = [
-    {
-        "label": lga,
-        "method": "update",
-        "args": [
-            {"visible": [lga == lg for lg in lga_labels]},
-            {"title": f"Business Trends for {lga} <br><sup><i>Active agricultural businesses at the end of each financial year</i></sup>"}
-        ]
-    }
-    for lga in lga_labels
-]
-
-# Add option to show all
-dropdown_buttons.insert(0, {
-    "label": "Select Region",
-    "method": "update",
-    "args": [
-        {"visible": [True] * len(lga_labels)},
-        {"title": "Business Trends in FNQ <br><sup><i>Active agricultural businesses at the end of each financial year</i></sup>"}
-    ]
-})
-
-# Apply updates to layout
+# Update layout
 fig.update_layout(
-    updatemenus=[
-        {
-            "active": 0,
-            "buttons": dropdown_buttons,
-            "direction": "down",
-            "showactive": True,
-            "x": 1.1,
-            "xanchor": "left",
-            "y": 1.15,
-            "yanchor": "top"
-        }
-    ],
+    font=dict(
+        family="Geist, sans-serif", 
+        size=14,  
+        color="slategrey"
+    ),
     yaxis=dict(
         title="Businesses",
-        tickformat=",",  # Adds commas for thousands
+        tickformat=",", 
     ),
     legend_title_text='Region',
-    margin=dict(l=40, r=40, t=60, b=40)
+    margin=dict(l=100, r=100, t=100, b=150),
+    bargap=0.5,          
+    bargroupgap=0.15 
+)
+
+# Add total figure to the top of each bar
+total = df.groupby("year")["total_business_count"].sum().reset_index()
+fig.add_trace(
+    go.Scatter(
+        x=total["year"],
+        y=total["total_business_count"],
+        text=total["total_business_count"],
+        mode="text",
+        textposition="top center",
+        showlegend=False,
+        cliponaxis=False
+    )
+)
+
+# Add data reference
+fig.add_annotation(
+    text="<a href='https://www.abs.gov.au/statistics/economy/business-indicators/counts-australian-businesses-including-entries-and-exits/latest-release' target='_blank'>Source: Australian Bureau of Statistics (Jul2020-Jun2024), Counts of Australian Businesses including Entries and Exits</a>",
+    xref="paper", yref="paper",
+    x=0.5, y=-0.2, 
+    showarrow=False,
+    font=dict(size=12, color="slategrey"),
+    align="center"
 )
 
 # Export the figure as html
-pio.write_html(fig, file="interactive_business_count.html", full_html=True, include_plotlyjs='cdn')
+pio.write_html(fig, file="plot_3_fnq_business_counts.html", full_html=True, include_plotlyjs='cdn')
