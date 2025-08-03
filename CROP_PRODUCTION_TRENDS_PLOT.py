d1d67fcb283d73c32ad02c3d73e250a4fcf71b96
naomiturner23 <ntur0008@student.monash.edu>
2025-08-03 11:40:15 +1000

    Update CROP_PRODUCTION_TRENDS_PLOT.py
    Altered colour scheme

--- CROP_PRODUCTION_TRENDS_PLOT.py
+++ CROP_PRODUCTION_TRENDS_PLOT.py
@@ -1,4 +1,4 @@
-## DATA VISUALISATION - ABS_CABEE_BY_LGA
+## DATA VISUALISATION - CROP_PRODUCTION_TRENDS
 
 # Import libraries
 import pandas as pd
@@ -30,6 +30,7 @@ df['cost_per_tonne'] = df['production_tonnes']/df['farm_gate_value_in_millions']
 # Create a pie chart
 fig = px.pie(df, names='region_label', values='production_tonnes',
               title="Fruit and Vegetable Production in Australia 2024 <br><sup><i>Measured in tonnes</i></sup>",
+              color_discrete_sequence= ['#d6f8d6','#a4e6a4','#6fdc6f','#34b434','#2a8f2a','#206b20','#184f18'],
               template="plotly_white")
 
 # Update layout
@@ -67,9 +68,10 @@ df_qld = df_qld.groupby("financial_year")["cost_per_tonne"].sum().reset_index()
 df_qld['cost_per_tonne'] = df_qld['cost_per_tonne'].round(0).astype(int)
 
 # Create a bar chart
-fig = px.line(df_qld, x='financial_year', y='cost_per_tonne',
+fig2 = px.line(df_qld, x='financial_year', y='cost_per_tonne',
               title="Queensland Fruit and Vegetables - Farm Gate Value by Tonne <br><sup><i>Measured in millions</i></sup>",
               labels={"financial_year": "Financial Year", "cost_per_tonne": "Value per Tonne (millions $)"},
+              color_discrete_sequence= ['#34b434'],
               template="plotly_white")
 
 # Add buffer to y axis
@@ -77,10 +79,10 @@ max_y = df_qld["cost_per_tonne"].max()
 min_y = df_qld["cost_per_tonne"].min()
 buffer_max = max_y * 0.1
 buffer_min = min_y * 0.1
-fig.update_yaxes(range=[min_y - buffer_min, max_y + buffer_max])
+fig2.update_yaxes(range=[min_y - buffer_min, max_y + buffer_max])
 
 # Update layout
-fig.update_layout(
+fig2.update_layout(
     font=dict(
         family="Geist, sans-serif", 
         size=14,  
@@ -91,25 +93,25 @@ fig.update_layout(
     bargroupgap=0.15 
 )
 
-fig.add_trace(go.Scatter(
+fig2.add_trace(go.Scatter(
     x=df_qld['financial_year'],
     y=df_qld['cost_per_tonne'],
     mode='markers',           
     name='Farm Gate Value per Tonne',
     showlegend=False,
-    line=dict(color='blue', width=2),
-    marker=dict(size=8, color='blue') 
+    line=dict(color='#34b434', width=2),
+    marker=dict(size=8, color='#34b434') 
 ))
 
 # Add data reference
-fig.add_annotation(
+fig2.add_annotation(
     text="<a href='https://www.abs.gov.au/statistics/industry/agriculture/australian-agriculture-horticulture/latest-release' target='_blank'>Source: Australian Bureau of Statistics (2023-24), Australian Agriculture: Horticulture</a>",
     xref="paper", yref="paper",
     x=0.5, y=-0.2, 
     showarrow=False,
-    font=dict(size=12, color="slategrey"),
+    font=dict(size=12, color="#1ca81c"),
     align="center"
 )
 
 # Export the figure as html
-pio.write_html(fig, file="plot_2_crop_production_value.html", full_html=True, include_plotlyjs='cdn')
+pio.write_html(fig2, file="plot_2_crop_production_value.html", full_html=True, include_plotlyjs='cdn')
